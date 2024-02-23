8380f85ea177a3c3191a27a0a57155fd22f30c9a
Dennis Chung <18393696+hikariNTU@users.noreply.github.com>
2024-02-23 16:06:47 +0800

    feat: add video format support via ffmpeg

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Media Converter
 
+![Media Converter Icon](icon.png)
+
 ## Features
 
-Convert images files into desire format for later usage.
+Convert image/video files into desired format for later usage.
 
 ## Requirements
 
-no?
+If video conversions are needed, the system must install `ffmpeg` in terminal through node exec command.
+
+https://www.ffmpeg.org/download.html
 
 ## Extension Settings
 
@@ -14,10 +18,11 @@ This extension contributes the following settings:
 
 - `vscode-media-converter.imageTargetFormat`: Target format for image files, if sharp support it.
 - `myExtension.imageQuality`: Image Sharp quality setting. A number between 0 to 100.
+- `vscode-media-converter.videoCommandDefault`: The command used for video conversion. INPUT_FILE will be replace by the file path, and OUTPUT_FILE will be replace by the file path WITHOUT extension
 
 ## Known Issues
 
-File is not checked before processing
+File types are not checked before processing.
 
 ## Release Notes
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,9 @@
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
 import * as vscode from "vscode";
 import sharp from "sharp";
+
+const exec_async = promisify(exec);
 
 type FileEntry = {
   fsPath: string;
@@ -8,6 +12,8 @@ type FileEntry = {
   scheme: "file";
 };
 
+const videoFormat = ["mov", "mp4", "wav", "mpeg", "mkv", "avi"];
+
 export function activate(context: vscode.ExtensionContext) {
   let converter = vscode.commands.registerCommand(
     "vscode-media-converter.convertMedia",
@@ -15,29 +21,62 @@ export function activate(context: vscode.ExtensionContext) {
       const configs = vscode.workspace.getConfiguration(
         "vscode-media-converter"
       );
-      const format = configs.get("imageTargetFormat") as string;
+      const imageFormat = configs.get("imageTargetFormat") as string;
+      const videoCommandDefault = configs.get("videoCommandDefault") as string;
       const quality = configs.get("imageQuality");
 
       const [selectedFile, files] = args;
       const result: string[] = [];
 
-      for (const file of files) {
-        const chunks = file.fsPath.split(".");
-        chunks.pop();
-        chunks.push(format);
-        const newPath = chunks.join(".");
+      vscode.window.withProgress(
+        {
+          location: vscode.ProgressLocation.Window,
+          cancellable: false,
+          title: `Converting ${files.length} Files`,
+        },
+        async (progress) => {
+          progress.report({ increment: 0 });
 
-        sharp(file.fsPath)
-          .toFormat(format as any, {
-            quality: typeof quality === "number" ? quality : 80,
-          })
-          .toFile(newPath);
+          let videoCommand = "";
 
-        result.push(newPath);
-      }
+          for (const file of files) {
+            const chunks = file.fsPath.split(".");
+            const originalFormat = chunks.pop();
 
-      vscode.window.showInformationMessage(
-        `Selected ${result.length} file(s) has been converted to "${format}" format with quality-${quality}.`
+            progress.report({ message: file.path });
+
+            if (videoFormat.includes(originalFormat || "")) {
+              const pathWithoutExt = chunks.join(".");
+              if (!videoCommand) {
+                videoCommand =
+                  (await vscode.window.showInputBox({
+                    title: "Provide ffmpeg convert command:",
+                    value: videoCommandDefault,
+                    prompt: "available variable: INPUT_FILE OUTPUT_FILE",
+                  })) || videoCommandDefault;
+              }
+              await exec_async(
+                videoCommand
+                  .replace("INPUT_FILE", file.fsPath)
+                  .replace("OUTPUT_FILE", pathWithoutExt)
+              );
+              result.push(pathWithoutExt);
+            } else {
+              chunks.push(imageFormat);
+              const newPath = chunks.join(".");
+              await sharp(file.fsPath)
+                .toFormat(imageFormat as any, {
+                  quality: typeof quality === "number" ? quality : 80,
+                })
+                .toFile(newPath);
+              result.push(newPath);
+            }
+            progress.report({ increment: (1 / files.length) * 100 });
+          }
+          vscode.window.showInformationMessage(
+            `Selected ${result.length} file(s) has been converted.`
+          );
+        }
       );
     }
   );
