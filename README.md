f51fcd53c7e7e6517c62c9d3928f078553794c96
Dennis Chung <18393696+hikariNTU@users.noreply.github.com>
2024-02-26 13:19:31 +0800

    fix: presets for processing
    - using presets configs for command
    - make sharp exclude from es build

--- README.md
+++ README.md
@@ -4,27 +4,23 @@
 
 ## Features
 
-Convert image/video files into desired format for later usage.
+Convert image/video files into desired formats.
 
 ## Requirements
 
-If video conversions are needed, the system must install `ffmpeg` in terminal through node exec command.
+`ffmpeg` is required for video processing.
 
 https://www.ffmpeg.org/download.html
-
-## Extension Settings
-
-This extension contributes the following settings:
-
-- `vscode-media-converter.imageTargetFormat`: Target format for image files, if sharp support it.
-- `myExtension.imageQuality`: Image Sharp quality setting. A number between 0 to 100.
-- `vscode-media-converter.videoCommandDefault`: The command used for video conversion. INPUT_FILE will be replace by the file path, and OUTPUT_FILE will be replace by the file path WITHOUT extension
 
 ## Known Issues
 
 File types are not checked before processing.
 
 ## Release Notes
+
+### 0.0.3
+
+Using presets configs for processing.
 
 ### 0.0.1
 
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import { exec } from "node:child_process";
 import { promisify } from "node:util";
 import * as vscode from "vscode";
 import sharp from "sharp";
+import parser from "yargs-parser";
 
 const exec_async = promisify(exec);
 
@@ -14,16 +15,115 @@ type FileEntry = {
 
 const videoFormat = ["mov", "mp4", "wav", "mpeg", "mkv", "avi"];
 
+async function getVideoPreset() {
+  const configs = vscode.workspace.getConfiguration("vscode-media-converter");
+  const presets = configs.get("videoCommandPresets") as string[];
+
+  let command = (
+    await vscode.window.showQuickPick(
+      [
+        ...presets.map((preset) => {
+          const presetChunk = preset.split("::");
+          return {
+            label: presetChunk[0],
+            detail: presetChunk.at(-1),
+          } as vscode.QuickPickItem;
+        }),
+        {
+          label: "Custom Command...",
+          detail: undefined,
+        } as vscode.QuickPickItem,
+      ],
+      { placeHolder: "Choose a preset for video format" }
+    )
+  )?.detail;
+
+  if (!command) {
+    command = await vscode.window.showInputBox({
+      placeHolder: "provide custom ffmpeg command...",
+      prompt: "available variable: INPUT_FILE OUTPUT_FILE",
+    });
+  }
+
+  return command;
+}
+
+async function getImagePreset() {
+  const configs = vscode.workspace.getConfiguration("vscode-media-converter");
+  const presets = configs.get("imagePresets") as string[];
+
+  let selected = await vscode.window.showQuickPick(
+    [
+      ...presets.map((preset) => {
+        const presetChunk = preset.split("::");
+        const [format, ...optionStr] = presetChunk.at(-1)!.split(" ");
+
+        return {
+          label: presetChunk[0],
+          description: format,
+          detail: optionStr.join(" "),
+        } as vscode.QuickPickItem;
+      }),
+      {
+        label: "Custom Command...",
+      } as vscode.QuickPickItem,
+    ],
+    { placeHolder: "Choose a preset for image format" }
+  );
+
+  const command = {
+    format: selected?.description,
+    options: selected?.detail,
+  };
+  if (!command.format) {
+    command.format = await vscode.window.showQuickPick(
+      [
+        "jpeg",
+        "png",
+        "webp",
+        "gif",
+        "jp2",
+        "tiff",
+        "avif",
+        "heif",
+        "jxl",
+        "raw",
+      ],
+      {
+        placeHolder: "Select output format",
+      }
+    );
+    if (!command.format) {
+      throw Error("No format selected");
+    }
+    command.options = await vscode.window.showInputBox({
+      placeHolder: "(optional) provide format options, eg: --quality=75",
+    });
+  }
+
+  const options = !command.options ? undefined : parser(command.options);
+
+  if (options && "_" in options) {
+    // @ts-expect-error remove command
+    delete options._;
+  }
+
+  return {
+    format: command.format,
+    options,
+  };
+}
+
 export function activate(context: vscode.ExtensionContext) {
   let converter = vscode.commands.registerCommand(
     "vscode-media-converter.convertMedia",
     (...args: [FileEntry, FileEntry[]]) => {
-      const configs = vscode.workspace.getConfiguration(
-        "vscode-media-converter"
-      );
-      const imageFormat = configs.get("imageTargetFormat") as string;
-      const videoCommandDefault = configs.get("videoCommandDefault") as string;
-      const quality = configs.get("imageQuality");
+      if (!args.length) {
+        vscode.window.showErrorMessage(
+          "Please Select and execute command through context menu in file explorer."
+        );
+        return;
+      }
 
       const [selectedFile, files] = args;
       const result: string[] = [];
@@ -38,22 +138,18 @@ export function activate(context: vscode.ExtensionContext) {
           progress.report({ increment: 0 });
 
           let videoCommand = "";
+          let imageFormat = "";
+          let imageOptions = undefined;
 
           for (const file of files) {
             const chunks = file.fsPath.split(".");
             const originalFormat = chunks.pop();
-
+            const pathWithoutExt = chunks.join(".");
             progress.report({ message: file.path });
 
             if (videoFormat.includes(originalFormat || "")) {
-              const pathWithoutExt = chunks.join(".");
               if (!videoCommand) {
-                videoCommand =
-                  (await vscode.window.showInputBox({
-                    title: "Provide ffmpeg convert command:",
-                    value: videoCommandDefault,
-                    prompt: "available variable: INPUT_FILE OUTPUT_FILE",
-                  })) || videoCommandDefault;
+                videoCommand = (await getVideoPreset()) || "";
               }
               await exec_async(
                 videoCommand
@@ -62,12 +158,16 @@ export function activate(context: vscode.ExtensionContext) {
               );
               result.push(pathWithoutExt);
             } else {
-              chunks.push(imageFormat);
-              const newPath = chunks.join(".");
+              if (!imageFormat) {
+                const { format, options } = await getImagePreset();
+                imageFormat = format;
+                imageOptions = options;
+              }
+
+              const newPath = `${pathWithoutExt}.${imageFormat}`;
+
               await sharp(file.fsPath)
-                .toFormat(imageFormat as any, {
-                  quality: typeof quality === "number" ? quality : 80,
-                })
+                .toFormat(imageFormat as any, imageOptions as any)
                 .toFile(newPath);
               result.push(newPath);
             }
